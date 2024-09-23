605821cdf8e041d1ff8d0e93b0ce962076b91e6f
SeSMP <se.santamaria@duocuc.cl>
2024-09-23 17:44:23 -0300

    Primera version de app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.appassitencia',
+  appName: 'appassitencia',
+  webDir: 'www'
+};
+
+export default config;
